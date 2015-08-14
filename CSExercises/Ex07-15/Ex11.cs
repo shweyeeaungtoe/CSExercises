30ac8fe1e5b576475ce028845dde5e4ce5efb717
shweyeeaungtoe <miss.shweyeeaungtoe@gmail.com>
2015-08-14 18:24:34 +0800

    finished

--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -10,7 +10,15 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+
+            double minimum_fixed_charge = 2.40;
+             
+            double distance;
+            System.Console.Write("Input the distance travelled :\t");
+            distance = Convert.ToDouble(System.Console.ReadLine());
+
+            double cal_distance = 2.40 + distance * 0.4;
+            System.Console.WriteLine("The distance travelled :\t"+ cal_distance);
         }
     }
 }
